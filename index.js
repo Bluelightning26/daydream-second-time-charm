f63073ce57b0bfe81a2b8b623117b1d5ddaeac7e
blu <87286496+Bluelightning26@users.noreply.github.com>
2025-09-27 16:18:01 -0400

    a

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class SacrificeClicker {
     constructor() {
 
-        this.doubloons = 1; // Start with a billion doubloons for testing
+        this.doubloons = 10000; // Start with a billion doubloons for testing
         this.totalClicks = 0;
         this.doubloonsPerClick = 1;
         this.doubloonsPerSecond = 0;
