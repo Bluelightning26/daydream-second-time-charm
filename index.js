c8f29438f62a8567c2efc8b2ca87e9f674e269a4
blu <87286496+Bluelightning26@users.noreply.github.com>
2025-09-27 15:49:18 -0400

    colors!!!!

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 class SacrificeClicker {
     constructor() {
 
-        this.doubloons = 10e6; // Start with a billion doubloons for testing
-
-        this.doubloons = 10000000; // Start with a billion doubloons for testing
+        this.doubloons = 1; // Start with a billion doubloons for testing
         this.totalClicks = 0;
         this.doubloonsPerClick = 1;
         this.doubloonsPerSecond = 0;
